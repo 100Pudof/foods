da656345f23ff9720f40d0db154c690a87701cdd
100Pudof <Xyebljadina@mail.ru>
2021-05-15 03:23:06 +0300

    z

--- src/redux/redusers/cartReduserCopy.js
+++ src/redux/redusers/cartReduserCopy.js
@@ -5,28 +5,24 @@ const initialState = {
     totalPrice: 0,
     totalCount: 0,
 }
-const getArr = (obj) => {
-    return [].concat.apply([], Object.values(obj));
-}
+const getArr = (obj) =>  [].concat.apply([], Object.values(obj));
 const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0)
 
 const cart = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_ITEM_CART': {
             let id = action.payload.id + '_' + action.payload.price;
-            const newItems = {
+            let newItems = {
                 ...state.items,
                 [id]: !state.items[id]  
                 ? [action.payload] 
                 : [...state.items[id], action.payload],
             };
-
-            const items = getArr(newItems);
-            const allPizzasTotalPrice = getTotalPrice(items);
+            let items = getArr(newItems);
             return {
                 ...state,
                 items: newItems,
-                totalPrice: allPizzasTotalPrice,
+                totalPrice: getTotalPrice(items),
                 totalCount: items.length
             }
         };
@@ -37,6 +33,7 @@ const cart = (state = initialState, action) => {
               };
               funcPlus[action.payload].push(funcPlus[action.payload][0]);
               const arr = getArr(funcPlus);
+              console.log(funcPlus[action.payload])
             return {
                 ...state,
                 items: funcPlus,
@@ -47,66 +44,46 @@ const cart = (state = initialState, action) => {
 
         case 'MINUS_ITEM': {
             const oldItems = state.items[action.payload];
-            const newObj =  oldItems.length > 1 
+            let newObj =  oldItems.length > 1 
             ? oldItems.slice(0, oldItems.length - 1) 
             : oldItems; 
-            const newItems = {
+            let minItems = {
                 ...state.items,
                 [action.payload]: newObj,
             }
-            
-             const arr = getArr(newItems);
+             const arr = getArr(minItems);
             return {
                 ...state,
-                items: newItems,
+                items: minItems,
                 totalCount: arr.length,
                 totalPrice: getTotalPrice(arr)
-
             }
         };
 
         case 'REMOVE_CART_ITEM': {
-            let newItems = {
+            let oldItems = {
                 ...state.items
             };
-                delete newItems[action.payload]
-                let arr = getArr(newItems)
+                delete oldItems[action.payload]
+                const arr = getArr(oldItems)
             return {
                 ...state,
-                items: newItems,
+                items: oldItems,
                 totalPrice: getTotalPrice(arr),
                 totalCount: arr.length,
             }
         }
 
         case 'CLEAR_CART': {
-            
             return {
                 ...state,
                 items: {},
                 totalPrice: null,
                 totalCount: null,
-
             }
         }
-
-
-
-
-        
         default:
             return state;
     }
 }
-
-// produce(initialState, draft => {
-        //     draft.items += Object.assign( {}, action.payload);
-        //     draft.totalPrice += action.payload.price;
-        //     draft.totalCount = draft.items.length;
-
-        //     // const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum, 0)
-        //     // oldPrice.reduce((sum, obj) => action.payload.price + sum, 0);
-        // })
-
-
 export default cart;
